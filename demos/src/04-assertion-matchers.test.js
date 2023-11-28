1b0202002866ee49c46aa90575626e54d27bd51c
ErikaMasacheM <92096749+ErikaMasacheM@users.noreply.github.com>
2023-11-28 16:53:10 -0500

    casos de prueba

--- demos/src/04-assertion-matchers.test.js
+++ demos/src/04-assertion-matchers.test.js
@@ -3,16 +3,34 @@ test('test obj', () => {
   data.lastname = 'molina';
   expect(data).toEqual({ name: 'nico', lastname: 'molina' });
 });
-test('test obj', () => {
+test('test null', () => {
   const data = null;
   expect(data).toBeNull();
   expect(data).toBeDefined();
   expect(data).not.toBeUndefined();
 });
 
-test('test obj', () => {
-  const data = null;
-  expect(data).toBeNull();
-  expect(data).toBeDefined();
-  expect(data).not.toBeUndefined();
+test('test booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+// ika esta contenida en Erika
+test('test strings', () => {
+  expect('Erika').toMatch(/ika/);
+});
+
+test('test booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+
+test('list / array', () => {
+  const numbers = [1, 3];
+  expect(numbers).toContain(3);
 });
---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,27 @@
+describe('set 1', () => {
+  beforeAll(() => {
+    console.log('before all');
+  });
+  test('case 1', () => {
+    console.log('caso 1');
+    expect(1 + 1).toBe(2);
+  });
+  test('case 2', () => {
+    console.log('caso 2');
+    expect(1 + 2).toBe(3);
+  });
+
+  describe('set 2', () => {
+    beforeAll(() => {
+      console.log('se ejecuta antes del segundo set');
+    });
+    test('caso 3', () => {
+      console.log('caso 3');
+      expect(1 + 4).toBe(5);
+    });
+    test('caso 4', () => {
+      console.log('caso 4');
+      expect(1 + 4).toBe(5);
+    });
+  });
+});
